ccdc29017ec6a68d9240892b43a1dcdac0f68d98
fine-agent <fine@fine.dev>
2025-03-16 18:18:22 +0000

    Add toast export to use-toast.ts hook

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,3 +1,5 @@
 import { useToast as useToastUI } from "@/components/ui/use-toast";
+import { toast as toastUI } from "@/components/ui/use-toast";
 
 export const useToast = useToastUI;
+export const toast = toastUI;
